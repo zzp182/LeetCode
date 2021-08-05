4bb53541d860bceaf433c0faaacb17862b7e65f2
zzp812875759 <zhangzhengpeng2021@163.com>
2021-08-05 16:07:33 +0800

    更新第7题

--- test.js
+++ test.js
@@ -1,6 +1,29 @@
-var removeDuplicates = function(nums) {
-    console.log(Array.from(new Set(nums)).length) 
+
+var longestPalindrome = function(s) {
+    if( s.length < 2){
+        return s   
+    }
+    let start = 0;
+    let maxlength =1;
+    function getHuiString(left,right){
+        while(left>=0 && right < s.length && s[left] === s[right]){
+            if(right - left + 1 > maxlength){
+                maxlength = right - left + 1
+                start = left
+            }
+            left--;
+            right++;
+        }  
+    }
+    for(var i=0;i<s.length;i++){
+        getHuiString(i-1 , i+1)
+        getHuiString(i,i+1)
+    }
+    return s.substring(start ,start+maxlength)
+    
 };
 
-var arr = [1,1,2]
-removeDuplicates(arr)
+str = 'abcdcbf'
+str1= 'aaaa'
+console.log(longestPalindrome(str))
+console.log(longestPalindrome(str1))
